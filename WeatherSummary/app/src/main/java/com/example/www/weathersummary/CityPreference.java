1ef16b2a0eb5b0fd31d5181d3d32a790c8ce9ef3
Ravikant <ravibih16@gmail.com>
2015-12-11 18:59:53 +0530

    first commit

---
+++ WeatherSummary/app/src/main/java/com/example/www/weathersummary/CityPreference.java
@@ -0,0 +1,22 @@
+package com.example.www.weathersummary;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+public class CityPreference {
+	
+	SharedPreferences prefs;
+	
+	public CityPreference(Activity activity){
+		prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+	}
+	
+	String getCity(){
+		return prefs.getString("city", "Delhi, IN");
+	}
+	
+	void setCity(String city){
+		prefs.edit().putString("city", city).commit();
+	}
+	
+}
